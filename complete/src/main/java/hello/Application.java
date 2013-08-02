6f89cb2272e7497000fea2195052d8e0d389293a
Roy Clarkson <rclarkson@gopivotal.com>
2013-08-02 00:16:59 -0400

    Update to latest Spring Boot configuration

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,7 +1,7 @@
 package hello;
 
-import org.springframework.bootstrap.SpringApplication;
-import org.springframework.bootstrap.context.annotation.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
