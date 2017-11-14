68885a1b36f9ca5c16c8ac97b44cd1d837f4b87e
Rajdash <31182494+Rajdash@users.noreply.github.com>
2017-11-14 16:57:17 +0530

    Update Smoketest.sh

--- Smoketest.sh
+++ Smoketest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-STATUS=`curl -s -o /dev/null -w "%{http_code}" http://34.234.85.118:8181`
+STATUS=`curl -s -o /dev/null -w "%{http_code}" http://34.229.130.233:8181`
 if [ ${STATUS} == 200 ]
 then 
 echo " APPLICATION IS UP AND RUNNING"
