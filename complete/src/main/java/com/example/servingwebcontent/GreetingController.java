cdf98b8951d54123fd5d28efd7d18ac2e3bb7949
Jay Bryant <jbryant@pivotal.io>
2019-10-03 17:00:17 -0500

    Use the Spring Initializr
    I rewrote the document and changed the code (mostly changing package names) to use the Spring Initializr.

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,4 +1,4 @@
-package hello;
+package com.example.servingwebcontent;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ initial/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContentApplication.class, args);
+	}
+
+}
