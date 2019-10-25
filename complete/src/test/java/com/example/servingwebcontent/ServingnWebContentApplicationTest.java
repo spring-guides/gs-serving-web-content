f98b9dcd8c7f2dea9ee89fc1d7d13764a726ba2c
Jay Bryant <jbryant@pivotal.io>
2019-10-25 11:57:17 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to Spring Boot 2.2.0, which necessitated changing the tests to work with JUnit 5 and replacing the Gradle wrappers. I also edited the README.adoc file to account for the DevTools dependency, which I had not called out in the earlier revision.

--- complete/src/test/java/com/example/servingwebcontent/ServingnWebContentApplicationTest.java
+++ complete/src/test/java/com/example/servingwebcontent/ServingnWebContentApplicationTest.java
@@ -20,14 +20,12 @@ import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(controllers = GreetingController.class)
 public class ServingnWebContentApplicationTest {
 
