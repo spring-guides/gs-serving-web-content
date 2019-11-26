e9b8f39d72526c76144841f4113a0467e2f65b83
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:09:21 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
 
 }
--- complete/src/test/java/hello/ApplicationTest.java
+++ complete/src/test/java/hello/ApplicationTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *	  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -31,26 +31,26 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest(controllers = GreetingController.class)
 public class ApplicationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Test
-    public void homePage() throws Exception {
-        // N.B. jsoup can be useful for asserting HTML content
-        mockMvc.perform(get("/index.html"))
-                .andExpect(content().string(containsString("Get your greeting")));
-    }
+	@Test
+	public void homePage() throws Exception {
+		// N.B. jsoup can be useful for asserting HTML content
+		mockMvc.perform(get("/index.html"))
+				.andExpect(content().string(containsString("Get your greeting")));
+	}
 
-    @Test
-    public void greeting() throws Exception {
-        mockMvc.perform(get("/greeting"))
-                .andExpect(content().string(containsString("Hello, World!")));
-    }
+	@Test
+	public void greeting() throws Exception {
+		mockMvc.perform(get("/greeting"))
+				.andExpect(content().string(containsString("Hello, World!")));
+	}
 
-    @Test
-    public void greetingWithUser() throws Exception {
-        mockMvc.perform(get("/greeting").param("name", "Greg"))
-                .andExpect(content().string(containsString("Hello, Greg!")));
-    }
+	@Test
+	public void greetingWithUser() throws Exception {
+		mockMvc.perform(get("/greeting").param("name", "Greg"))
+				.andExpect(content().string(containsString("Hello, Greg!")));
+	}
 
 }
