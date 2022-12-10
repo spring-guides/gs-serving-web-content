01c06d900f734b46b2b93a9d7dade5e9d3b0bf8e
ChrisKolep <kolep.krisztian@gmail.com>
2022-12-10 14:23:59 +0100

    Titkos üzenet száll a széllel.

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="Szasz") String name, Model model) {
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="Home") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
