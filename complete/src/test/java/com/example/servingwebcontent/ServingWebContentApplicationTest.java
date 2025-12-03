3996ecca0cd93817e6265f1fae1f2405352a02b7
Brian Clozel <brian.clozel@broadcom.com>
2025-12-03 17:48:44 +0100

    Upgrade to Spring Boot 4.0

--- complete/src/test/java/com/example/servingwebcontent/ServingWebContentApplicationTest.java
+++ complete/src/test/java/com/example/servingwebcontent/ServingWebContentApplicationTest.java
@@ -16,17 +16,19 @@
 
 package com.example.servingwebcontent;
 
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-
 @WebMvcTest(controllers = GreetingController.class)
+@AutoConfigureMockMvc
 public class ServingWebContentApplicationTest {
 
 	@Autowired
