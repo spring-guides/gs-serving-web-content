499e59bf2931c5b438c3b2a01ccd05ef685d6634
Danil Pavlov <danil.pavlov@jetbrains.com>
2026-03-19 14:22:37 +0100

    Update README.adoc for Kotlin
    Signed-off-by: Danil Pavlov <danil.pavlov@jetbrains.com>

--- complete-kotlin/src/main/kotlin/com/example/servingwebcontent/GreetingController.kt
+++ complete-kotlin/src/main/kotlin/com/example/servingwebcontent/GreetingController.kt
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam
 @Controller
 class GreetingController {
 
-	@GetMapping("/greeting")
-	fun greeting(@RequestParam(name = "name", required = false, defaultValue = "World") name: String, model: Model): String {
-		model.addAttribute("name", name)
-		return "greeting"
-	}
-
+    @GetMapping("/greeting")
+    fun greeting(
+        @RequestParam(name = "name", required = false, defaultValue = "World") name: String,
+        model: Model
+    ): String {
+        model.addAttribute("name", name)
+        return "greeting"
+    }
 }
